59d5d5001a0cfe7f3b26ad29ecd212fd60b5398f
caesiumy <mn0658@naver.com>
2019-02-26 23:44:41 +0900

    feat: Add blog/home (not yet completed)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.core.paginator import Paginator
+from .models import Post
 
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, 'home.html')
