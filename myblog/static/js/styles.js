9f21f252876c137d847c5b496c823903ea9f5c55
caesiumy <mn0658@naver.com>
2019-03-16 22:36:44 +0900

    fix: Fix dropdown

--- myblog/static/js/styles.js
+++ myblog/static/js/styles.js
@@ -19,4 +19,7 @@ $(document).ready(function () {
     $('.close').on('click', function () {
         $(this).closest('.message').transition('fade');
     });
+
+    $('.ui.dropdown')
+        .dropdown();
 });
